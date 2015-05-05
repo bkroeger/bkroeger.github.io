189fbbf629cc7b01da924a2547466d8cd2f496f4
Brad Kroeger <bkroeger@shaw.ca>
2015-05-04 21:00:48 -0600

    version 1.0

--- javascript/adder.js
+++ javascript/adder.js
@@ -1,36 +1,43 @@
 window.view = {
 	
-	displayAnswer: function(answer){
-	    $('#adderModal').modal('show');
-	    $('#adderDiv').html('<div class="alert alert-danger">' + answer + '</div>');		
-	}
+  displayAnswer: function(answer){
+    $('#adderModal').modal('show');
+    $('#adderDiv').html('<div class="alert alert-danger">' + answer + '</div>');		
+  },
+	
+  addErrorStyle: function(input){
+    input.parent('div').addClass('has-error');
+  },
+  
+  removeErrorStyle: function(input){
+    input.parent('div').removeClass('has-error');
+  }
 	
 }
 
 window.controller = {
 
-    initPage: function() {
-        $('#addButton').click(controller.addButton);
-	},
-		
+  initPage: function() {
+    $('#addButton').click(controller.addButton);
+  },		
 
-	addButton: function(){
-	  var input = $('#addInput');
-	  var number = parseInt(input.val());
-	  if(number < 1){
-	     input.parent('div').addClass('has-error');
-	  }else{
-        input.parent('div').removeClass('has-error');	  
-		view.displayAnswer(model.add(number));
-	  }
-    }		
+  addButton: function(){
+    var input = $('#addInput');
+    var number = parseInt(input.val());
+    if(number < 1){
+      view.addErrorStyle(input);
+    }else{
+      view.removeErrorStyle(input);	  
+      view.displayAnswer(model.add(number));
+    }
+  }		
 
 }
 
 window.model = {
 
-    add: function(number){
-	return number > 0 ? (number * (number + 1)) / 2 : 0;
-    }
+  add: function(number){
+    return number > 0 ? number * (number + 1) / 2 : 0;
+  }
 }
 
